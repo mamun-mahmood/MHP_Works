76f3095a16d1228b5c6937bdb15219fc17017991
Khannakunal32 <khannakunal32@gmail.com>
2021-08-25 16:05:38 +0530

    Kunal ProfileEdit SettingPopup to main

--- components/NewMessageButton/newContactDot/newContactdot.tsx
+++ components/NewMessageButton/newContactDot/newContactdot.tsx
@@ -1,19 +1,85 @@
-import React from "react";
-import { View,TouchableOpacity } from "react-native";
-import { AntDesign,MaterialIcons } from '@expo/vector-icons'; 
-import styles from './style'
-import { useNavigation } from "@react-navigation/core";
-const NewContactDot = () => {
-     const navigation =useNavigation();
+// import React from "react";
+// import { View,TouchableOpacity } from "react-native";
+// import { AntDesign,MaterialIcons } from '@expo/vector-icons'; 
+// import styles from './style'
+// import { useNavigation } from "@react-navigation/core";
+// const NewContactDot = () => {
+//      const navigation =useNavigation();
  
 
-  const onClick =()=>{
-  navigation.navigate('Contacts')
-    }
+//   const onClick =()=>{
+//   navigation.navigate('Contacts')
+//     }
 
-  return <TouchableOpacity onPress={onClick} ><View >
-<MaterialIcons name="contact-page" size={22} color="white" />
-  </View></TouchableOpacity> 
+//   return <TouchableOpacity onPress={onClick} ><View >
+// <MaterialIcons name="contact-page" size={22} color="white" />
+//   </View></TouchableOpacity> 
+// };
+
+// export default NewContactDot;
+
+//kunal
+
+
+import { View, TouchableOpacity } from "react-native";
+import { AntDesign, MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
+import styles from "./style";
+import { useNavigation } from "@react-navigation/core";
+import React, { useState } from 'react';
+
+import { Text } from 'react-native';
+import { Menu, MenuItem, MenuDivider } from 'react-native-material-menu';
+
+
+const NewContactDot = () => {
+  const navigation = useNavigation();
+
+  const onClick = () => {
+    navigation.navigate("Contacts");
+  };
+  const OpenSettings = () => {
+    navigation.navigate("Settings");
+  };
+  const OpenContacts = () => {
+    navigation.navigate("Contacts");
+  };
+ 
+
+  const [visible, setVisible] = useState(false);
+
+  const hideMenu = () => setVisible(false);
+
+  const showMenu = () => setVisible(true);
+
+  return (
+    <TouchableOpacity  onPress={showMenu} hitSlop={{top: 20, bottom: 20, left: 50, right: 50}}>
+    <View style={{ height: '100%', alignItems: 'center', justifyContent: 'center' }}>
+        {/* <MaterialIcons name="contact-page" size={22} color="white" /> */}
+        <Menu style={{width: 180}}
+                visible={visible}
+                anchor={
+                  <MaterialCommunityIcons
+                    name="dots-vertical"
+                    size={25}
+                    color={"white"}
+                   
+                    
+                  />
+                  
+                }
+                onRequestClose={hideMenu}
+              >
+                <MenuItem onPress={hideMenu}>Account</MenuItem>
+                <MenuItem onPress={hideMenu}>New Group</MenuItem>
+                <MenuItem >Invite</MenuItem>
+                <MenuItem onPress={OpenContacts}>Contacts</MenuItem>
+                <MenuItem onPress={OpenSettings}>Settings</MenuItem>
+                <MenuDivider />
+                {/* <MenuItem onPress={hideMenu}>Menu item 4</MenuItem> */}
+              </Menu>
+      </View>
+     </TouchableOpacity>
+  );
 };
 
 export default NewContactDot;
